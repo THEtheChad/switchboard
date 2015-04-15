371a89443d42dde0e39ce74eba814fd92bf27a15
THEtheChad <chad.d.elliott@gmail.com>
2015-04-14 19:56:36 -0500

    added simple example

--- example/simple.js
+++ example/simple.js
@@ -1,6 +1,6 @@
 /*
- * node-template
- * https://github.com/THEtheChad/node-template
+ * switchboard
+ * https://github.com/THEtheChad/switchboard
  *
  * Copyright (c) 2015 Chad Elliott
  * Licensed under the MIT license.
@@ -8,6 +8,30 @@
 
 'use strict';
 
-var nodeTemplate = require('../');
+var Switchboard = require('../');
 
-console.log(nodeTemplate()); // "awesome"
+var switchboard = new Switchboard();
+
+switchboard.directory({
+	'Harry S Truman': '922-122-1333',
+	'Paul White': '777-244-3333'
+});
+
+switchboard.contact('Charlie Brown', '421-212-1212');
+
+switchboard.action('Phone Home', function(){
+	console.log('Calling outerspace....');
+});
+
+switchboard.action('Prank Call', function(){
+	console.log('Heyuk heyuk heyuk');
+});
+
+var carol = switchboard.operator('Carol');
+
+carol
+	.caller('Harry S Truman')
+		.then('Prank Call')
+  .caller('Paul White')
+  .caller('Charlie Brown')
+		.then('Phone Home');
